768c3112ea0c779fd941fbd5205482bd96aafb04
Mykhailo <sharkon90@gmail.com>
2024-01-03 20:05:58 -0500

    adding addEmployee function to this case. Moving all functions to separet .js files, ficinx add role function

--- assets/sql/seeds.sql
+++ assets/sql/seeds.sql
@@ -15,14 +15,14 @@ INSERT INTO role (title, salary, department_ID) VALUES
 ("Lawyer", 190000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-("Jhon", "Doe", 1, 1),
-("Mike", "Chan", 2, 1),
-("Ashley", "Rodriguez", 3, 1),
-("Kevin", "Tupik", 4, 3),
-("Kunal", "Singh", 5, 1),
-("Malia", "Brown", 6, 1),
-("Sarah", "Lourd", 7, 1),
-("Tom", "Allen", 8, 1);
+("Jhon", "Doe", 1, 0),
+("Mike", "Chan", 4, 1),
+("Ashley", "Rodriguez", 2, 0),
+("Kevin", "Tupik", 3, 4),
+("Kunal", "Singh", 7, 0),
+("Malia", "Brown", 6, 8),
+("Sarah", "Lourd", 5, 0),
+("Tom", "Allen", 8, 0);
 
 SELECT * FROM department;
 SELECT * FROM role;
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const fs = require("fs");
-const Role = require("./lib/roles");
-const Department = require("./lib/departments");
-const Employees = require("./lib/employees");
-const { response } = require("express");
-const departmantArray = ['Engineering', 'Finance']
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'rootroot',
-        database: 'emloyee_db'
-    },
-    console.log("Welcom to Employee Manager")
-);
+const viewEmployees = require("./lib/viewEmployees");
+const addEmployees = require("./lib/addEmployees");
+const updateEmployee = require("./lib/updateEmployee");
+const viewRoles = require("./lib/viewRoles");
+const addRole = require("./lib/addRole");
+const addDepartment = require("./lib/addDepartment");
+const viewDepartment = require("./lib/viewDepartment");
+
+
+
 
 const init = function () {
     inquirer.prompt([
@@ -28,128 +22,31 @@ const init = function () {
         .then((data) => {
             switch (data.toDo) {
                 case "View All Employees":
-
-                    db.query("SELECT * FROM employee JOIN role ON employee.role_id = role.id", (err, res) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            let arr = [];
-                            res.forEach((el) => {
-                                let newRole = new Employees(el.first_name, el.last_name, el.title, el.salary);
-                                arr.push(newRole);
-                            })
-                            console.table(arr);
-                            init();
-                        }
-                    });
+                    viewEmployees(init);
                     break;
+
                 case "Add Employee":
-                    // inquirer.prompt([
-                    //     {
-                    //         type: "input",
-                    //         massage:
-                    //     }
-                    // ])
+                    addEmployees(init);
                     break;
 
                 case "Update Employee Role":
-                    console.log(data.toDo);
-                    init();
+                    updateEmployee(init);
                     break;
 
                 case "View All Roles":
-                    db.query("SELECT * FROM role JOIN department ON role.department_ID = department.id", (err, res) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            let arr = [];
-                            res.forEach((el) => {
-                                let newRole = new Role(el.title, el.salary, el.name);
-                                arr.push(newRole);
-                            })
-                            console.table(arr);
-                            init();
-                        }
-                    });
+                    viewRoles(init);
                     break;
 
                 case "Add Role":
-                    inquirer.prompt([
-                        {
-                            type: 'input',
-                            name: 'roleName',
-                            message: 'What is the name of the role?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'roleSalary',
-                            message: 'What is the salary of the role?',
-                        },
-                        {
-                            type: 'list',
-                            name: 'department',
-                            message: 'Which department does the role belong to?',
-                            choices: departmantArray
-                        }
-                    ])
-                        .then((data) => {
-                            console.log(data);
-                            db.query(`INSERT INTO role (title, salary) VALUES ("${data.roleName}", ${data.roleSalary})`, (err, response) => {
-                                if (err) {
-                                    console.error(err);
-                                } else {
-                                    db.query(`SELECT * FROM role`, (err, response) => console.error(err));
-                                    init();
-                                }
-                            })
-                        })
+                    addRole(init);
                     break;
 
                 case "View All Dpartments":
-                    db.query("SELECT * FROM department", (err, res) => {
-                        if (err) {
-                            console.error(err);
-                        } else {
-                            let arr = [];
-                            res.forEach((el) => {
-                                let newRole = new Department(el.name);
-                                arr.push(newRole);
-                            })
-                            console.table(arr);
-                            init();
-                        }
-                    });
+                    viewDepartment(init);
                     break;
 
                 case "Add Department":
-                    inquirer.prompt([
-                        {
-                            type: 'input',
-                            name: 'departmantName',
-                            message: "What is the name of the department?"
-                        }
-                    ])
-                        .then((data) => {
-                            db.query(`INSERT INTO department (name) VALUES ("${data.departmantName}")`, (err, result) => {
-                                if (err) {
-                                    console.error(err);
-                                } else {
-                                    db.query("SELECT * FROM department", (err, res) => {
-                                        if (err) {
-                                            console.error(err);
-                                        } else {
-                                            let arr = [];
-                                            res.forEach((el) => {
-                                                let newRole = new Department(el.name);
-                                                arr.push(newRole);
-                                            })
-                                            console.table(arr);
-                                            init();
-                                        }
-                                    });
-                                }
-                            })
-                        })
+                    addDepartment(init);
                     break;
 
                 case "Exit":
