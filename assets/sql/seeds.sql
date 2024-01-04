3a32384d21089c7c57f5a3841f703f07196707e2
Mykhailo <sharkon90@gmail.com>
2024-01-04 14:34:25 -0500

    ficing viewEmployees

--- assets/sql/seeds.sql
+++ assets/sql/seeds.sql
@@ -15,14 +15,14 @@ INSERT INTO role (title, salary, department_ID) VALUES
 ("Lawyer", 190000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-("Jhon", "Doe", 1, 0),
+("Jhon", "Doe", 1, null),
 ("Mike", "Chan", 4, 1),
-("Ashley", "Rodriguez", 2, 0),
-("Kevin", "Tupik", 3, 4),
-("Kunal", "Singh", 7, 0),
+("Ashley", "Rodriguez", 2, null),
+("Kevin", "Tupik", 3, 6),
+("Kunal", "Singh", 7, null),
 ("Malia", "Brown", 6, 8),
-("Sarah", "Lourd", 5, 0),
-("Tom", "Allen", 8, 0);
+("Sarah", "Lourd", 5, null),
+("Tom", "Allen", 8, null);
 
 SELECT * FROM department;
 SELECT * FROM role;
